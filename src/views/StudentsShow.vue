a003110aa054f274bca41d543d7512f886d25121
kirill grinash <kirillgrinash@gmail.com>
2020-11-02 19:43:52 -0600

    updated data on studentsshow

--- src/views/StudentsShow.vue
+++ src/views/StudentsShow.vue
@@ -1,28 +1,41 @@
 <template>
   <div class="students-show">
       <div>
-        <h1>Prison Mike</h1>
-        <img src="https://pbs.twimg.com/profile_images/1050414908762939393/UKzYsgQg_400x400.jpg" alt="Prison Mike"/>
+        <h1>{{ user.first_name }}</h1>
+        <img v-bind:src="user.photo" v-bind:alt="user.first_name"/>
         <button>Contact Info</button><br>
-        <span>prisonmike@theoffice.com</span>
-        <span>(123) 456 - 7890</span>
+        <span>{{ user.email }}</span>
+        <span>{{ user.phone }}</span>
       </div>
+
       <section id="short-bio">
-        Jim Halpert : What did you do, Prison Mike? Michael Scott : [pretending he went to prison] I stole and I robbed and I kidnapped the President’s son and held him for ransom. Jim Halpert : That is quite the rap sheet, Prison Mike.
+        <p>{{ user.shortBio }}</p>
+
       </section>
       
       <section id='experience'>
-        <span id="start_date">12-12-2018</span>
-        <span id="end_date">11-12-2020</span>
-        <span id="job_title"></span>
+        <span id="start_date">{{ user.experience.start_date }}</span>
+        <span id="end_date">{{ user.experience.end_date }}</span>
+        <span id="job_title">{{ user.experience.job_title }}</span>
+        <span id="company_name">{{ user.experience.company_name }}</span>
+        <span id="details">{{ user.experience.details }}</span>
+
       </section>
       <section id='education'>
-      
+        <span id="start_date">{{ user.education.start_date }}</span>
+        <span id="end_date">{{ user.education.end_date }}</span>
+        <span id="degree">{{ user.education.degree }}</span>
+        <span id="university_name">{{ user.education.university_name }}</span>
+        <span id="details">{{ user.education.details }}</span>
+
       </section>
       <section id='projects'>
+        <span id="name">{{ user.projects.name }}</span>
+        <span id="description">{{ user.projects.description }}</span>
       
       </section>
       <section id='skills'>
+        <span id="name">{{ user.skills.name }}</span>
       
       </section>
       
@@ -36,58 +49,20 @@ img {
 </style>
 
 <script>
-const students = [
-  {
-    firstName: "Prison",
-    lastName: "Mike",
-    email: "prisonmike@theoffice.com",
-    phone: "1234567890",
-    shortBio:
-      "Jim Halpert : What did you do, Prison Mike? Michael Scott : [pretending he went to prison] I stole and I robbed and I kidnapped the President’s son and held him for ransom. Jim Halpert : That is quite the rap sheet, Prison Mike.",
-    linkedInURL: "https://linkedin.com",
-    twitterHandle: "prisonmike",
-    website: "https://website.com",
-    resumeURL: "https://resume.com",
-    github: "https://github.com",
-    photo: "https://pbs.twimg.com/profile_images/1050414908762939393/UKzYsgQg_400x400.jpg",
-  },
-  {
-    first_name: "Bob",
-    last_name: "Smith",
-    email: "prisonmike@theoffice.com",
-    phone_number: "1234567890",
-    short_bio:
-      "Jim Halpert : What did you do, Prison Mike? Michael Scott : [pretending he went to prison] I stole and I robbed and I kidnapped the President’s son and held him for ransom. Jim Halpert : That is quite the rap sheet, Prison Mike.",
-    linkedin_url: "https://linkedin.com",
-    twitter_handle: "prisonmike",
-    website_url: "https://website.com",
-    online_resume_url: "https://resume.com",
-    github_url: "https://github.com",
-    photo: "https://pbs.twimg.com/profile_images/1050414908762939393/UKzYsgQg_400x400.jpg",
-  },
-  {
-    firstName: "John",
-    lastName: "Mike",
-    email: "prisonmike@theoffice.com",
-    phone: "1234567890",
-    shortBio:
-      "Jim Halpert : What did you do, Prison Mike? Michael Scott : [pretending he went to prison] I stole and I robbed and I kidnapped the President’s son and held him for ransom. Jim Halpert : That is quite the rap sheet, Prison Mike.",
-    linkedInURL: "https://linkedin.com",
-    twitterHandle: "prisonmike",
-    website: "https://website.com",
-    resumeURL: "https://resume.com",
-    github: "https://github.com",
-    photo: "https://pbs.twimg.com/profile_images/1050414908762939393/UKzYsgQg_400x400.jpg",
-  },
-];
+import axios from "axios";
 
 export default {
   data: function () {
     return {
-      students: students,
+      user: {},
     };
   },
-  created: function () {},
+  created: function () {
+    axios.get("/api/users/" + this.$route.params.id).then(response => {
+      console.log("users show", response);
+      this.user = response.data;
+    });
+  },
   methods: {},
 };
 </script>
